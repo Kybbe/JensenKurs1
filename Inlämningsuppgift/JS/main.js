1fce2cb7c016003ba188f44d3fdc440b59be7ca0
Jacob Klaren <33457831+Kybbe@users.noreply.github.com>
2021-09-26 17:32:47 +0200

    Super big final stretch of final assessment
    added entire contact, galler, live and sponsor sections / sites.
    removed sponsor.htmland changed it out for sponsor section on homepage

--- Inlämningsuppgift/JS/main.js
+++ Inlämningsuppgift/JS/main.js
@@ -37,13 +37,18 @@ if(selectedTheme == null || selectedTheme == undefined){
     setTheme(localStorage.getItem('theme'))
 }
 
-$(".flippableCard").mousedown( function() {
+$(".flippableCard").mousedown( function(e) {
+    if(e.target.className == "cardBodyButton") {
+        return;
+    }
     card = $(this);
 
-    if (card.className == "card flippedCard") {
+    if ($(card).attr('class') == "card flippableCard flippedCard") {
         $(card).removeClass("flippedCard")
+        $(card).children().children(".flip-card-back").show(1).delay(500).hide(1);
     } else {
         $(card).addClass("flippedCard");
+        $(card).children().children(".flip-card-back").hide(1).delay(500).show(1);
     }
 })
 
@@ -58,14 +63,41 @@ function openNavbar() {
 function submitResult() {
     var form = document.getElementById("contactForm");
     var inputs = form.getElementsByTagName("input");
-    var resultDiv = document.getElementById("result")
+    var resultDiv = document.getElementById("result");
+    var select = document.getElementById("boatClass");
+    var checkbox = document.getElementById("terms");
     var result = "";
+    var invalid = "";
     var allValid = true;
 
     for(inputThingy of inputs) { // get all inputs
-        result += inputThingy.id + ": " + inputThingy.value + " \r\n"; //set result as a alert friendly formed list
+        if(inputThingy.id == "terms") {
+            result += "Has read Privacy policy:" + " " + inputThingy.value + " \r\n";
+        } else if(inputThingy.value == "Get values") {
+
+        } else {
+            result += inputThingy.attr('placeholder') + " " + inputThingy.value + " \r\n"; //set result as a alert friendly formed list
+        }
         if(!inputThingy.checkValidity()) {
             allValid = false;
+            inputThingy.style.backgroundColor = "red";
+            inputThingy.style.color = "white";
+
+            if(invalid == "") {
+                invalid += inputThingy.attr('placeholder');
+            } else {
+                invalid += ", \r\n " + inputThingy.attr('placeholder')
+            }
+        }
+    }
+    result += $('label[for="' + select.id + '"]').html() + select.value + " \r\n";
+    if(!checkbox.checked){
+        allValid = false;
+
+        if(invalid == "") {
+            invalid += checkbox.id;
+        } else {
+            invalid += ", \r\n" + checkbox.id
         }
     }
 
@@ -73,13 +105,16 @@ function submitResult() {
         if (window.confirm(result + " \r\n" + "Click 'ok' if all info above looks right.")) {// visar alla inputs och frågar användaren om dom vill se svaret på hemsidan också
             resultDiv.innerHTML = "Form sent successfully!"; // skriv ut svaret i div "result"
             resultDiv.style.color = "green";
+
+            for(inputThingy of inputs) {
+                inputThingy.backgroundColor = "unset"
+            }
         };
     } else {
+        alert("Please fill in the following fields correctly: \r\n" + invalid)
         resultDiv.innerHTML = "Form not fully filled out or valid.";
         resultDiv.style.color = "red";
     }
 
-    
-    
 }
 
